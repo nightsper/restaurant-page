634eac571a076aaf9e78718f0536e64256f5862f
Nightsper <fahimhossain645@gmail.com>
2025-12-09 18:49:34 +0000

    Removed image file

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
     module: {
         rules: [
             {
-            test: /\.html$/i,
-            loader: "html-loader",
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
             },
         ],
     },
